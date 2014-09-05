4c552b905cc01a37e51da8485974562985b862b8
Warren Gavin <warren@dubelyoo.com>
2014-09-05 06:46:40 -0500

    Initial commit

---
+++ git-install.sh
@@ -0,0 +1,7 @@
+git config --global user.name "Warren Gavin"
+git config --global user.email "warren@dubelyoo.com"
+git config --global core.autocrlf input
+git config --global core.safecrlf true
+git config --global core.editor vim
+git config --global merge.tool vimdiff
+git config --global color.ui auto
