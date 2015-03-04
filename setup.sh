d95baa6e251acd62d8ab0614bc8ebca45ce298f2
Warren Gavin <warren@dubelyoo.com>
2015-03-04 14:06:03 -0600

    updated

--- setup.sh
+++ setup.sh
@@ -1,24 +1,66 @@
 #!/bin/sh
 
-# Initialze home directory with dotfiles.
-# DO SOMETHING HERE...
+# Copy all a few of our dotfiles in to our home directory and adding
+# /usr/local/bin & /usr/local/opt/coreutils/libexec/gnubin to utilize in this
+# script.
+
+echo
+echo Initializing the home directory with dotfiles.
+cp -r home/.[^.]* ~/
+export PATH="/usr/local/bin:$PATH"
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 
 # Installing Ruby with rbenv.
+
+echo
+echo Installing Ruby.
 rbenv install 2.2.0
 rbenv global 2.2.0
+
 # Initialize Ruby environment so gems are installed in the correct location.
+
+echo
+echo Initializing rbenv...
+
 eval "$(/usr/local/bin/rbenv init -)"
+
+echo
+echo Installing compass
 gem install compass
 
-# Installing nodejs with nvm
-mkdir ~/.nvm
+
+# Initialize nvm environment
+
+echo
+echo Initializing nvm...
+
+mkdir ~/.nvm # TODO add test for directory
 export NVM_DIR=~/.nvm
-# Initialize nodejs environment so node modules are installed in the correct location.
 source $(brew --prefix nvm)/nvm.sh
+
+# Installing nodejs with nvm
+
+echo
+echo Installing nodejs.
 
 nvm install v0.10.36
 nvm alias default v0.10.36
 nvm use default
+
+# Install node modules.
+
+echo
+echo Installing node modules.
+
+npm install -g \
+  yo \
+  grunt \
+  grunt-cli \
+  bower \
+  generator-angularfire \
+  firebase-tools
+
+npm cache clean
 
 echo
 echo Close this terminal and open a new one.
