4eaea936f03bfa37a26287dc8eac67b15363f91a
Warren Gavin <warren@dubelyoo.com>
2015-03-11 14:17:29 -0500

    Updated

--- initialize.sh
+++ initialize.sh
@@ -1,28 +1,13 @@
 #!/bin/sh
 
-
-function abort {
-  echo "$1"
-  exit 1
-}
+#
+# Bash script to install Homebrew, applications and some minor configurations
+# to utilize the new installed application.
+#
 
 set -e
 
-# Determine if the Xcode command line tools installed.
-
-XCODE_SELECT="$(xcode-select -p 2>&1)";
-
-if [[ $XCODE_SELECT == "/Library/Developer/CommandLineTools" || \
-  $XCODE_SELECT == "/Applications/Xcode.app/Contents/Developer" ]]
-  then
-    echo "Xcode command line tools installed."
-  else
-    echo "Xcode is not installed."
-    xcode-select --install
-fi
-
-command -v /usr/bin/git >/dev/null 2>&1 || abort "Xcode command line tools git not found!" 
-
+# Instll homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew update
@@ -47,6 +32,7 @@ brew install nmap
 brew install nvm
 brew install rbenv
 brew install ruby-build
+brew install ctags
 brew install caskroom/cask/brew-cask
 
 brew cask install clamxav
@@ -57,30 +43,21 @@ brew cask install makemkv
 brew cask install spotify
 brew cask install the-unarchiver
 brew cask install google-chrome
-#brew cask install parallels-desktop
 brew cask install mailbox
 brew cask install silverlight
 brew cask install gopro-studio
 brew cask install real-vnc
-#brew cask install cleanmymac
-#brew cask install snagit
 brew cask install dropbox
 brew cask uninstall crashplan
-#brew cask install sublime-text
 brew cask install transmission
+brew cask install microsoft-office
+brew cask install atom
+#brew cask install parallels-desktop
 #brew cask install minecraft
-#brew cask install 
+#brew cask install sublime-text
+#brew cask install cleanmymac
+#brew cask install snagit
 
 brew cleanup
-
-# Add the homebrew version of bash to /etc/shells
-
-if ! grep -Fxq "/usr/local/bin/bash" /etc/shells ; then
-  echo "/usr/local/bin/bash" >> /etc/shells
-fi
-
-# Change the default shell for the current user
-
-chsh -s /usr/local/bin/bash
 
 exit 0
