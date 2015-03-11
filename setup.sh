bc946dae44afb8d2ab597f81a895abb49e54e431
Warren Gavin <warren@dubelyoo.com>
2015-03-11 14:17:53 -0500

    Updated

--- setup.sh
+++ setup.sh
@@ -10,6 +10,30 @@ cp -r home/.[^.]* ~/
 export PATH="/usr/local/bin:$PATH"
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 
+# Add the homebrew version of bash to /etc/shells
+
+if ! grep -Fxq "/usr/local/bin/bash" /etc/shells ; then
+  echo "/usr/local/bin/bash" >> /etc/shells
+fi
+
+# Change the default shell for the current user
+
+chsh -s /usr/local/bin/bash
+
+# Future enhancements:
+# - Pass name & email as arguments
+
+# The following is my personal settings for git.
+
+git config --global user.name "Warren Gavin"
+git config --global user.email "warren@dubelyoo.com"
+git config --global core.autocrlf input
+git config --global core.safecrlf true
+git config --global core.editor vim
+git config --global merge.tool vimdiff
+git config --global color.ui auto
+git config --global push.default simple
+
 # Installing Ruby with rbenv.
 
 echo
