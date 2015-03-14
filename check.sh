822aa0b34da21d20723ecee546c2ea13ba949e58
Warren Gavin <warren@dubelyoo.com>
2015-03-13 22:35:47 -0500

    Updated

--- check.sh
+++ check.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
+
+# Setting some color constants
 COLOR_GREEN="\x1b[0;32m"
 COLOR_RED="\x1b[0;31m"
 COLOR_RESET="\x1b[0m"
 
+# Setting some escaped characters
 CHAR_CHECKMARK="\xE2\x9C\x93"
 CHAR_XMARK="\xE2\x9C\x97"
 
+# Host used to test Internet connection
 PING_HOST="raw.githubusercontent.com"
 #PING_HOST="madeup.di.namemadszxcv.com"
 
@@ -15,13 +19,11 @@ function abort {
   exit 1
 }
 
-set -e
-
 echo
 echo "Checking a few things to make sure we are good to go..."
 echo
 
-
+# Checking for an Internet connection
 if /sbin/ping -s1 -t4 -o ${PING_HOST} &> /dev/null
   then
     echo "${COLOR_GREEN}${CHAR_CHECKMARK}${COLOR_RESET} We have an Internet connection."
@@ -31,24 +33,26 @@ fi
 
 # Determine if the Xcode command line tools installed.
 
+# Checking to make sure xcode-command line tools are installed.
 XCODE_SELECT=$(xcode-select -p 2>&1);
 
 if [[ $XCODE_SELECT == "/Library/Developer/CommandLineTools" || \
   $XCODE_SELECT == "/Applications/Xcode.app/Contents/Developer" ]]
   then
     echo "${COLOR_GREEN}${CHAR_CHECKMARK}${COLOR_RESET} Xcode command line tools installed."
-    
+
   else
     echo "${COLOR_RED}${CHAR_XMARK}${COLOR_RESET} Xcode command line tools not installed."
     abort "Please run xcode-select --install"
 fi
 
+# Checking for git
 if command -v /usr/bin/git >/dev/null 2>&1
   then
     echo "${COLOR_GREEN}${CHAR_CHECKMARK}${COLOR_RESET} git appears to be available."
   else
     abort "${COLOR_RED}${CHAR_XMARK}${COLOR_RESET} Xcode command line tools git not found!"
-fi 
+fi
 
 echo
 echo "${COLOR_GREEN}${CHAR_CHECKMARK}${COLOR_RESET} All good to go..."
