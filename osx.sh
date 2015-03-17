3f24f84eba37e0bb532b1190e2298860c9fbafb6
Warren Gavin <warren@dubelyoo.com>
2015-03-16 22:43:04 -0500

    Updated

--- osx.sh
+++ osx.sh
@@ -21,4 +21,7 @@ chflags nohidden ~/Library
 # Set the default and magnification icon sizes
 defaults write com.apple.dock magnification -bool false
 
+# Add Chrome
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Google Chrome.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+
 killall Dock
