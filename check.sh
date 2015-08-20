037711ce8f83eccd5c8fd288ce96601a5e2b837b
Warren Gavin <warren@dubelyoo.com>
2015-08-20 10:34:05 -0500

    Updated

--- check.sh
+++ check.sh
@@ -31,6 +31,14 @@ if /sbin/ping -s1 -t4 -o ${PING_HOST} &> /dev/null
     abort "${COLOR_RED}${CHAR_XMARK}${COLOR_RESET} We do not have an Internet connection."
 fi
 
+# Check if the current account is in the admin group
+if groups | grep -w -q admin 2>&1
+  then
+    echo "${COLOR_GREEN}${CHAR_CHECKMARK}${COLOR_RESET} You are an admin."
+  else
+    abort "${COLOR_RED}${CHAR_XMARK}${COLOR_RESET} You are not an admin."
+fi
+
 # Determine if the Xcode command line tools installed.
 
 # Checking to make sure xcode-command line tools are installed.
