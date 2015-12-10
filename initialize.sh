d4eba3d77003fdf718f0a929ac3b6a9a6c4c66d9
Warren Gavin <warren@dubelyoo.com>
2015-12-10 07:25:31 -0600

    Updated

--- initialize.sh
+++ initialize.sh
@@ -7,6 +7,12 @@
 
 set -e
 
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Install homebrew
 ruby -e "$(curl -fsSL \
     https://raw.githubusercontent.com/Homebrew/install/master/install)"
