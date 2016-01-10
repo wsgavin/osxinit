956b4477376270a08a70e737b26ae6b90f412bc7
Warren Gavin <warren@dubelyoo.com>
2016-01-10 09:41:23 -0600

    Updated

--- init/sudo_keep_alive.sh
+++ init/sudo_keep_alive.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 # Ask for the administrator password upfront.
-echo "$sudo_password" | sudo -Sv
+#echo "$sudo_password" | sudo -Sv
 
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+while true; do
+  #sudo -n true;
+  echo "$sudo_password" | sudo -Sv
+  sleep 60;
+  kill -0 "$$" || exit;
+done 2>/dev/null &
