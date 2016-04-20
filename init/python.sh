dfb2f7dca38cc7694cf3f675819ba02fd3ef7e92
Warren Gavin <warren@dubelyoo.com>
2016-04-20 15:53:51 -0500

    Updated

--- init/python.sh
+++ init/python.sh
@@ -1,21 +1,36 @@
 #!/bin/sh
 
 
-brew install python
+brew install pyenv
+
+eval "$(pyenv init -)"
+
+echo
+echo "Installing python..."
+
+pyenv install 2.7.11
+pyenv install 3.5.1
 
 
 echo
 echo "Initializing python..."
 
-mkdir -p "$HOME/Library/Python/2.7/lib/python/site-packages"
-echo 'import site; site.addsitedir("/usr/local/lib/python2.7/site-packages")' >> \
-  "$HOME/Library/Python/2.7/lib/python/site-packages/homebrew.pth"
+#mkdir -p "$HOME/Library/Python/2.7/lib/python/site-packages"
+#echo 'import site; site.addsitedir("/usr/local/lib/python2.7/site-packages")' >> \
+#  "$HOME/Library/Python/2.7/lib/python/site-packages/homebrew.pth"
 
 mkdir -p "$HOME/.virtualenvs"
 
+pyenv local 2.7.11
+
 pip install --upgrade --no-use-wheel pip setuptools
-#pip install --upgrade setuptools
-pip install virtualenv
 pip install virtualenvwrapper
+
+pyenv local 3.5.1
+
+pip install --upgrade --no-use-wheel pip setuptools
+pip install virtualenvwrapper
+
+pip global 2.7.11
 
 echo "done."
